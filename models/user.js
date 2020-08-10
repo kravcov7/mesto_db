fb3c0bb87c7d5d29d360854c91b4613ebfdf0787
Artem Kravtsov <dr.artem-k@yandex.com>
2020-08-10 18:36:39 +0400

    созданы схемы и модели, отредактировал package.json

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validate: (link) => validator.isURL(link),
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
