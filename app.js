7995905679fba861101f51519c87fa5c8357ae11
Artem Kravtsov <dr.artem-k@yandex.com>
2020-08-11 17:14:53 +0400

    удалил data, public. подключил body-parser. создал тестого пользователя

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 /* eslint-disable eol-last */
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
-const path = require('path');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
@@ -16,7 +16,17 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5f32983d4599453b488eea61',
+  };
+
+  next();
+});
+
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 app.use((req, res) => {
